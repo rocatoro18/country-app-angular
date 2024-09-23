3e2b81276a6c1a9a5dcbdf1d82d8f9a731dad7b5
Roberto Tores <rocatoro18@outlook.com>
2024-09-23 16:53:40 -0700

    .

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    AboutPageComponent,
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    HomePageComponent,
+    AboutPageComponent
+  ]
+})
+export class SharedModule { }
